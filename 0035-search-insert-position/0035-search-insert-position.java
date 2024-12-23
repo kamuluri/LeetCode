c8f2803cc38436181b3c26a823e515fda5c30aec
kamuluri <133738856+kamuluri@users.noreply.github.com>
2024-12-23 18:51:09 +0530

    Time: 0 ms (100%), Space: 42.9 MB (66.11%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        // At this point, left > right, meaning the target is not found
+        // and the left pointer points to the index where the target
+        // should be inserted to maintain sorted order.
+        return left;
+    }
+}
