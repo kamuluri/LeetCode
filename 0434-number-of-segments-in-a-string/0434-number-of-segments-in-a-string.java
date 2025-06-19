9a43b6cc848413177bef8d9a10aab11c0ca771f4
kamuluri <133738856+kamuluri@users.noreply.github.com>
2025-06-19 15:03:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public int countSegments(String s) {
         if (s == null || s.trim().isEmpty()) return 0;
-        String[] segments = s.trim().split("\\s+");
+        String[] segments = s.trim().split(" ");
         return segments.length;
     }
 }
