9979dd4dab5477465c84a29b7b25bb3927efa72e
kamuluri <133738856+kamuluri@users.noreply.github.com>
2025-06-19 12:07:44 +0530

    Time: 1 ms (44.47%), Space: 40.9 MB (71.6%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int addDigits(int num) {
+        int sum=0;
+        do{
+            sum=0;
+            while(num>0){
+                int a=num%10;
+                sum+=a;
+                num/=10;
+            }
+            num=sum;
+        }while(sum>9);
+        return sum;
+    }
+}
